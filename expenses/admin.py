cbf6ba602673be42fde63aebd1279450af82e778
Javiercerna <javier.cerna@pucp.pe>
2019-01-24 21:58:51 -0500

    Add Expense models, move dashboard to main path

--- expenses/admin.py
+++ expenses/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import ExpenseType, ExpenseFinancing, Expense
 
-# Register your models here.
+
+class ExpenseTypeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+class ExpenseFinancingAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date', 'money_pen', 'money_usd', 'type', 'financing')
+
+
+admin.site.register(ExpenseType, ExpenseTypeAdmin)
+admin.site.register(ExpenseFinancing, ExpenseFinancingAdmin)
+admin.site.register(Expense, ExpenseAdmin)
--- expenses/models.py
+++ expenses/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class ExpenseType(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class ExpenseFinancing(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Expense(models.Model):
+    name = models.CharField(max_length=120)
+    date = models.DateField()
+    money_pen = models.FloatField()
+    money_usd = models.FloatField()
+    type = models.ForeignKey(ExpenseType, on_delete=models.CASCADE)
+    financing = models.ForeignKey(ExpenseFinancing, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return (f'{{"name": {self.name}, "date": {self.date}, "money_pen": {self.money_pen},'
+                f'"money_usd": {self.money_usd}, "type": {self.type}, "financing": {self.financing}}}')
