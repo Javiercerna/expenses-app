13a3d62fb349c94b176cd6e8a9d18c96cb5bb704
Javiercerna <javier.cerna@pucp.pe>
2019-01-31 22:26:42 -0500

    Add Django REST Framework, register REST for Expense

--- backend/expenses/views.py
+++ backend/expenses/views.py
@@ -1,4 +1,33 @@
+from django.contrib.auth.models import User, Group
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Expense
+from .serializers import UserSerializer, GroupSerializer, ExpenseSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class ExpenseViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Expense.objects.all()
+    serializer_class = ExpenseSerializer
+
 
 def index(request):
     return render(request, 'expenses/dashboard.html')
--- backend/expenses_app/urls.py
+++ backend/expenses_app/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework import routers
+from expenses import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+router.register(r'expenses', views.ExpenseViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('expenses.urls')),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
