17604a5c24cdb221f37674bd332812054197a6f8
Javiercerna <javier.cerna@pucp.pe>
2019-02-17 23:14:50 -0500

    Render initial graph: financing_expenses

---
+++ frontend/src/dashboard/ExpensesGraph.js
@@ -0,0 +1,68 @@
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import React, { Component } from 'react';
+
+import Plotly from 'plotly.js-dist';
+
+
+class ExpensesGraph extends Component {
+  constructor() {
+    super();
+    this.state = {
+      own_expenses: 0,
+      other_expenses: 0
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://127.0.0.1:8000/expenses/')
+    .then(results => {
+      return results.json();
+    }).then(data => {
+      let own_expenses = 0;
+      let other_expenses = 0;
+      data.results.forEach(expense => {
+        if (expense.financing === 'Propio') {
+          own_expenses += expense.money_pen;
+        } else if (expense.financing === 'Prestado') {
+          other_expenses += expense.money_pen;
+        }
+      });
+      this.setState({
+        own_expenses: own_expenses,
+        other_expenses: other_expenses
+      });
+      this.renderDataInGraph();
+    });
+  }
+
+  renderDataInGraph = () => {
+    let data = [
+      {
+        x: ['Personal', 'Prestado'],
+        y: [this.state.own_expenses, this.state.other_expenses],
+        marker: {
+          color: ['rgba(0,153,102,1)', 'rgba(255,222,51,1)']
+        },
+        type: 'bar'
+      }
+    ];
+
+    let layout = {
+      title: 'Modo de financiamiento',
+      plot_bgcolor: '#FBFBFB',
+      paper_bgcolor: "#FBFBFB",
+      bargap: 0.02
+    };
+
+    Plotly.newPlot(this.props.graph_name, data, layout);
+  };
+
+  render() {
+    return (
+      <div id={this.props.graph_name}></div>
+    );
+  }
+}
+
+export default ExpensesGraph;
--- frontend/src/dashboard/index.js
+++ frontend/src/dashboard/index.js
@@ -4,6 +4,7 @@ import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import Navbar from './Navbar';
 import NewExpenseModal from './NewExpenseModal';
+import ExpensesGraph from './ExpensesGraph';
 import ExpensesSummary from './ExpensesSummary';
 
 const styles = theme => ({
@@ -39,6 +40,18 @@ class Dashboard extends Component {
             <Button variant="contained" color="primary" onClick={this.handleClickOpen}>
               Registrar nuevo pago
             </Button>
+            <Grid container spacing={16}>
+              <Grid item xs={4}>
+                <ExpensesGraph graph_name="latest_expenses"/>
+              </Grid>
+              <Grid item xs={4}>
+                <ExpensesGraph graph_name="expenses_distribution"/>
+              </Grid>
+              <Grid item xs={4}>
+                <ExpensesGraph graph_name="expenses_financing"/>
+              </Grid>
+            </Grid>
+
             <NewExpenseModal open={this.state.modal_open} onClose={this.handleClose} />
             <ExpensesSummary />
           </Grid>
